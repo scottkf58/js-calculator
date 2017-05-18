63035e76950828da83d9a26dbf878db231578d94
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-05-17 21:13:27 -1000

    adding memory and setting total

--- calculator.js
+++ calculator.js
@@ -5,6 +5,9 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
+ function calculatorModule() {
+  var memory = 0;
+  var total = 0;
 
 
   /**
@@ -12,54 +15,108 @@
    * @param  { Number } x
    * @return { Number }    current total
    */
+   function load(x) {
+    validator(x);
+      total = x;
+      return total;
+   }
+
 
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
+   function getTotal() {
+    return total;
+   }
 
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
+   function add(x) {
+    validator(x);
+    total += x;
+   }
 
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
+   function subtract(x) {
+    validator(x);
+    total -= x;
+   }
 
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
+   function multiply(x) {
+    validator(x);
+    total *= x;
+   }
 
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
+   function divide(x) {
+    validator(x);
+    total /= x;
+   }
 
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
+   function getMemory() {
+    return memory;
+   }
 
 
   /**
    * Stores the value of `total` to `memory`
    */
+   function storeTotal() {
+    memory = total;
+   }
 
 
   /**
    * Clear the value stored at `memory`
    */
+   function clear() {
+    memory = 0;
+   }
 
   /**
    * Validation
    */
+   function validator(x) {
+    if(typeof x !== "number") {
+      throw Error();
+    }
+   }
+
+   return{
+    load: load,
+    getTotal: getTotal,
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+    recallMemory: getMemory,
+    saveMemory: storeTotal,
+    clearMemory: clear
+   };
+
+ }
+   
 
